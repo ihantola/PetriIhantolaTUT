b42efe101e29c6a20944afa7a41b700b3b5fbb50
Petri Ihantola <petri.ihantola@tut.fi>
2015-03-09 22:36:52 -0700

    Initial version

---
+++ js/site.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  function init() {
+  	$("#header").css({height: $(window).height()});
+
+    //$window.on('scroll', onScroll)
+    //$window.on('resize', resize)
+    //$popoverLink.on('click', openPopover)
+    //$document.on('click', closePopover)
+    //$('a[href^="#"]').on('click', smoothScroll)
+    //buildSnippets();
+  }
+
+init();
+
+});
